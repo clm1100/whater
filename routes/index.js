f6d8dc0a2dff6563d8224b9cf1fc5710c4d97fce
clm110 <275298711@qq.com>
2017-09-15 00:32:19 +0800

    确定首页页面布局样式

--- routes/index.js
+++ routes/index.js
@@ -3,6 +3,11 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
+  res.render('index/home',{ });
+});
+
+
+router.get('/list', function(req, res, next) {
   res.render('index/index',{ });
 });
 
